28a80406fa0692e6a886257c94846c41fa4c866c
mwisniewskii <150433@student.uwm.edu.pl>
2022-03-11 22:10:36 +0100

    PEP8 formatting.

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,3 +1,4 @@
+# Django
 from django.contrib.auth.base_user import BaseUserManager
 
 
@@ -6,10 +7,9 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
+
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
+        """Create and save a User with the given email and password."""
         if not email:
             raise ValueError('The Email must be set')
         email = self.normalize_email(email)
@@ -19,9 +19,7 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
+        """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
