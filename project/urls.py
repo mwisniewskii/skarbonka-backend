63aea4e299f945d73ed8f459cb758467edae7a11
mwisniewskii <150433@student.uwm.edu.pl>
2022-04-24 18:12:49 +0200

    static urls

--- project/urls.py
+++ project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 # Django
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
-from django.urls import re_path
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 # 3rd-party
 from drf_yasg import openapi
@@ -43,4 +44,4 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path("", include("skarbonka.urls")),
     path("", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
