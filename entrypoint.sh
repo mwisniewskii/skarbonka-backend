f2d8ac6e9692f0f80bd9765b7e0c57527e50274c
mwisniewskii <150433@student.uwm.edu.pl>
2022-04-24 19:25:12 +0200

    gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 poetry run python manage.py migrate
 poetry run python manage.py collectstatic --noinput
-poetry run python manage.py runserver 0.0.0.0:8000
+poetry run gunicorn project.wsgi:application --timeout 300 --bind 0.0.0.0:8000 --reload --access-logfile '-' --error-logfile '-'
